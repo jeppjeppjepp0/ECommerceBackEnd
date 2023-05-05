314c14442fd78ba51b384d49353dbd96bb9b7983
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-05-04 21:06:09 -0600

    models index updated

--- models/index.js
+++ models/index.js
@@ -15,8 +15,22 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: {
+        model: ProductTag,
+        unique: false,
+    },
+    as: 'product_tag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: {
+        model: ProductTag,
+        unique: false,
+    },
+    as: 'tag_product'
+});
 
 module.exports = {
   Product,
